3056bac4b7b83d86f72815ceafc6edd0d55a85a1
1021zhang <eleven1021zhang@gmail.com>
2026-06-22 08:53:38 +0800

    standardize sticker pack cover size

--- sticker-packs.js
+++ sticker-packs.js
@@ -55,6 +55,8 @@ const catY2KPackStickers = createStickerItems(
   ]
 );
 
+// Official sticker pack cover spec:
+// 1200x900, 4:3, safe area 80px, object-fit contain.
 /** @type {StickerPack[]} */
 const officialStickerPacks = [
   createOfficialStickerPack(
